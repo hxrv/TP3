1017d15dc2315b68def54499cbfb955d5966e282
hxrv <herveym@icloud.com>
2021-03-17 19:55:42 -0400

    Version 1

---
+++ pymafia/__init__.py
@@ -0,0 +1,10 @@
+"""Lorsqu'un dossier contient un module __init__.py, c'est que nous le considérons comme
+un "package", qui est une collection de modules regroupés dans un même projet.
+
+Ce module spécial permet de faire:
+from pymafia.partie import ...
+
+Aucun code n'est nécessaire dans ce module spécial, sa présence est suffisante.
+Si vous créez un package python dans Pycharm, vous obtiendrez automatiquement ce fichier.
+
+"""
